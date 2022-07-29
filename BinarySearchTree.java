b98e9f29689c566b4593904865cee57ddc0ac9a5
chutki12 <priyankachaudhary2068>
2022-07-29 22:09:43 +0530

    ADDED : Welcome To Binary Search Tree Program

---
+++ BinarySearchTree.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.binarySearchTree;
+
+public class BinarySearchTree {
+	
+
+	public static void main(String[] args) {
+		
+		System.out.println(" Welcome To Binary Search Tree ");
+
+	}
+
+}
